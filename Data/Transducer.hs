2eb5f3814b60748e312acb497d6207efa26044d2
Edward Kmett <ekmett@gmail.com>
2012-08-16 18:30:56 -0400

    we can now starve sources, cap pipes and tees with no type inference problems

--- Data/Transducer.hs
+++ Data/Transducer.hs
@@ -104,7 +104,7 @@ stop = empty
 starve :: SF k a b -> [b]
 starve Stop          = []
 starve (Emit o k)    = o : starve k
-starve (Receive _ f) = starve f
+starve (Receive _ _ f) = starve f
 
 -------------------------------------------------------------------------------
 -- Stream Transducers
@@ -112,40 +112,40 @@ starve (Receive _ f) = starve f
 
 data SF k i o
   = Emit o (SF k i o)
-  | Receive (k i (SF k i o)) (SF k i o)
+  | forall r. Receive (r -> SF k i o) (k i r) (SF k i o)
   | Stop
 
-transform :: Functor (k i) => (forall a. k i a -> k' i' a) -> SF k i o -> SF k' i' o
+transform :: (forall a. k i a -> k' i' a) -> SF k i o -> SF k' i' o
 transform f (Emit o k)    = Emit o (transform f k)
 transform _ Stop          = Stop
-transform f (Receive g h) = Receive (f (fmap (transform f) g)) (transform f h)
+transform f (Receive g kir h) = Receive (transform f . g) (f kir) (transform f h)
 
 runSF :: Functor (k i) => (o -> r -> r) -> (k i r -> r -> r) -> r -> SF k i o -> r
 runSF ke kr kf m = go m where
-  go (Emit o k)    = ke o (go k)
-  go (Receive f r) = kr (fmap go f) (go r)
-  go Stop          = kf
+  go (Emit o k)        = ke o (go k)
+  go (Receive f kir r) = kr (fmap (go . f) kir) (go r)
+  go Stop              = kf
 {-# INLINE runSF #-}
 
 compile :: SM k i o a -> SF k i o
-compile m = runSM m (const Stop) Emit Receive Stop
+compile m = runSM m (const Stop) Emit (Receive id) Stop
 
 repeatedly :: SM k i o a -> SF k i o
-repeatedly m = r where r = runSM m (const r) Emit Receive Stop
+repeatedly m = r where r = runSM m (const r) Emit (Receive id) Stop
 
 before :: SF k i o -> SM k i o a -> SF k i o
-before f m = runSM m (const f) Emit Receive Stop
+before f m = runSM m (const f) Emit (Receive id) Stop
 
 instance Category (SF (->)) where
   id = repeatedly $ do
      i <- receive
      emit i
 
-  Stop        . _            = Stop
-  Emit a as   . sf           = Emit a (as . sf)
-  Receive f _ . Emit b bs    = f b . bs
-  Receive _ k . Stop         = k . Stop
-  sf          . Receive g fg = Receive (\a -> sf . g a) (sf . fg)
+  Stop        . _                = Stop
+  Emit a as   . sf               = Emit a (as . sf)
+  Receive f kir _ . Emit b bs    = fmap f kir b . bs
+  Receive _ _ k . Stop           = k . Stop
+  sf          . Receive g kir fg = Receive (\a -> sf . g a) kir (sf . fg)
 
 fun :: (a -> b) -> Pipe a b
 fun f = repeatedly $ do
@@ -180,12 +180,12 @@ droppingWhile p = before id loop where
   loop = receive >>= \v -> if p v then loop else emit v
 
 -- | bolt a pipe on the end of any stream transducer
-pipe :: Functor (k a) => Pipe b c -> SF k a b -> SF k a c
-pipe Stop          _              = Stop
-pipe (Emit a as)   sf             = Emit a (pipe as sf)
-pipe (Receive f _) (Emit b bs)    = pipe (f b) bs
-pipe (Receive _ g) Stop           = pipe g Stop
-pipe sf            (Receive g fg) = Receive (fmap (pipe sf) g) (pipe sf fg)
+pipe :: Pipe b c -> SF k a b -> SF k a c
+pipe Stop               _                 = Stop
+pipe (Emit a as)       sf                 = Emit a (pipe as sf)
+pipe (Receive f kir _) (Emit b bs)        = pipe (fmap f kir b) bs
+pipe (Receive _ _ g)   Stop               = pipe g Stop
+pipe sf                (Receive g kir fg) = Receive (fmap (pipe sf) g) kir (pipe sf fg)
 
 buffered :: Int -> Pipe a [a]
 buffered = repeatedly . go [] where
@@ -197,18 +197,19 @@ buffered = repeatedly . go [] where
 feed :: [a] -> Pipe a b -> Pipe a b
 feed []     r             = r
 feed _      Stop          = Stop
-feed (x:xs) (Receive f _) = feed xs (f x)
+feed (x:xs) (Receive f kir _) = feed xs (fmap f kir x)
 feed xs     (Emit o k)    = Emit o (feed xs k)
 
 -------------------------------------------------------------------------------
 -- Source
 -------------------------------------------------------------------------------
 
-type Source b = forall k a. Functor (k a) => SF k a b
+type Source b = forall k a. SF k a b
 
 source :: Foldable f => f b -> Source b
 source xs = compile (traverse_ emit xs)
 
+-- | @'cap' = 'pipe'@
 cap :: Pipe a b -> Source a -> Source b
 cap l r = pipe l r
 
@@ -218,7 +219,7 @@ cap l r = pipe l r
 
 -- | \"Is that your final answer?\"
 sink :: (forall o. SM k i o a) -> SF k i a
-sink m = runSM m (\a -> Emit a Stop) id Receive Stop
+sink m = runSM m (\a -> Emit a Stop) id (Receive id) Stop
 
 -------------------------------------------------------------------------------
 -- Tees
@@ -245,20 +246,20 @@ passR = repeatedly $ do
   emit b
 
 liftL :: Pipe a c -> Tee a b c
-liftL = runSF Emit (Receive . L) Stop
+liftL = runSF Emit (Receive id . L) Stop
 
 liftR :: Pipe b c -> Tee a b c
-liftR = runSF Emit (Receive . R) Stop
+liftR = runSF Emit (Receive id . R) Stop
 
 tee :: Pipe a a' -> Pipe b b' -> Tee a' b' c -> Tee a b c
 tee a b (Emit c sf) = Emit c $ tee a b sf
 tee _ _ Stop        = Stop
-tee (Emit a sf) b (Receive (L f) _) = tee sf b (f a)
-tee Stop b (Receive L{} ff) = tee Stop b ff
-tee (Receive g fg) b (Receive (L f) ff) = Receive (L (\a -> tee (g a) b (Receive (L f) ff))) (tee fg b (Receive (L f) ff))
-tee a (Emit b sf) (Receive (R f) _) = tee a sf (f b)
-tee a Stop (Receive R{} ff) = tee a Stop ff
-tee a (Receive g fg) (Receive (R f) ff) = Receive (R (\b -> tee a (g b) (Receive (R f) ff))) (tee a fg (Receive (R f) ff))
+tee (Emit a sf) b (Receive f (L kf) _) = tee sf b (fmap f kf a)
+tee Stop b (Receive _ L{} ff) = tee Stop b ff
+tee (Receive g kg fg) b (Receive f (L kf) ff) = Receive id (L (\a -> tee (fmap g kg a) b (Receive f (L kf) ff))) (tee fg b (Receive f (L kf) ff))
+tee a (Emit b sf) (Receive f (R kf) _) = tee a sf (fmap f kf b)
+tee a Stop (Receive _ R{} ff) = tee a Stop ff
+tee a (Receive g kg fg) (Receive f (R kf) ff) = Receive id (R (\b -> tee a (fmap g kg b) (Receive f (R kf) ff))) (tee a fg (Receive f (R kf) ff))
 
 addL :: Pipe a b -> Tee b c d -> Tee a c d
 addL p = tee p id
