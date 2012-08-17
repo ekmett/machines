6413427724f90ec2422736396ad0bf8be369b090
Edward Kmett <ekmett@gmail.com>
2012-08-17 04:54:39 -0400

    after

--- src/Data/Machine/Process.hs
+++ src/Data/Machine/Process.hs
@@ -17,9 +17,8 @@ module Data.Machine.Process
   , ProcessT
   , Automaton(..)
   -- ** Common Processes
---  , after
+  , after
   , supply
---  , pipe
   , prepended
   , filtered
   , dropping
@@ -59,35 +58,36 @@ instance Automaton (->) where
     i <- await
     yield (f i)
 
--- | A process that prepends the elements of a 'Foldable' onto its input, then repeats its input from there.
+-- | A 'Process' that prepends the elements of a 'Foldable' onto its input, then repeats its input from there.
 prepended :: Foldable f => f a -> Process a a
 prepended = before id . traverse_ yield
 
--- | A process that only passes through inputs that match a predicate.
+-- | A 'Process' that only passes through inputs that match a predicate.
 filtered :: (a -> Bool) -> Process a a
 filtered p = repeatedly $ do
   i <- await
   when (p i) $ yield i
 
--- | A process that drops the first @n@, then repeats the rest.
+-- | A 'Process' that drops the first @n@, then repeats the rest.
 dropping :: Int -> Process a a
 dropping n = before id $ replicateM_ n await
 
--- | A process that passes through the first @n@ elements from its input then stops
+-- | A 'Process' that passes through the first @n@ elements from its input then stops
 taking :: Int -> Process a a
 taking n = construct . replicateM_ n $ await >>= yield
 
--- | A process that passes through elements until a predicate ceases to hold, then stops
+-- | A 'Process' that passes through elements until a predicate ceases to hold, then stops
 takingWhile :: (a -> Bool) -> Process a a
 takingWhile p = repeatedly $ await >>= \v -> if p v then yield v else stop
 
--- | A process that drops elements while a predicate holds
+-- | A 'Process' that drops elements while a predicate holds
 droppingWhile :: (a -> Bool) -> Process a a
 droppingWhile p = before id loop where
   loop = await >>= \v -> if p v then loop else yield v
 
 -- | Chunk up the input into `n` element lists.
--- The last list may be shorter.
+--
+-- Avoids returning empty lists and deals with the truncation of the last group.
 buffered :: Int -> Process a [a]
 buffered = repeatedly . go [] where
   go [] 0  = stop
@@ -96,24 +96,22 @@ buffered = repeatedly . go [] where
     i <- await <|> yield (reverse acc) *> stop
     go (i:acc) $! n-1
 
-{-
--- | Bolt a 'Process' on the end of any 'Machine'.
-pipe :: Process b c -> Machine k a b -> Machine k a c
-pipe Stop            _                = Stop
-pipe (Yield a as)    sf               = Yield a (pipe as sf)
-pipe (Await f kir _) (Yield b bs)     = pipe (fmap f kir b) bs
-pipe (Await _ _ g)   Stop             = pipe g Stop
-pipe sf              (Await g kir fg) = Await (fmap (pipe sf) g) kir (pipe sf fg)
--}
-
-{-
-after :: Machine k a b -> Process b c -> Machine k a c
-after _ Stop                            = Stop
-after sf (Yield a as)    = Yield a (after sf as)
-after (Yield b bs) (Await f kir _)  = after bs (fmap f kir b)
-after Stop (Await _ _ g)   = after Stop g
-after (Await g kir fg) sf = Await (fmap (`after` sf) g) kir (after fg sf)
--}
+-- | Build a new 'Machine' by adding a 'Process' to the output of an old 'Machine'
+--
+-- @
+-- after :: Process a b   -> Process b c -> Process a c
+-- after :: Tee a b c     -> Process c d -> Tee a b d
+-- after :: Machine k a b -> Process b c -> Machine k a c
+-- @
+after :: Monad m => MachineT m k a b -> ProcessT m b c -> MachineT m k a c
+after ma mp = MachineT $ runMachineT mp >>= \v -> case v of
+  Stop          -> return Stop
+  Yield o k     -> return $ Yield o (after ma k)
+  Await f kf ff -> runMachineT ma >>= \u -> case u of
+    Stop          -> runMachineT $ after stopped ff
+    Yield o k     -> runMachineT $ after k (fmap f kf o)
+    Await g kg fg -> let mv = MachineT (return v) in
+      return $ Await (fmap (`after` mv) g) kg (after fg mv)
 
 -- | Feed a 'Process' some input.
 supply :: Monad m => [a] -> ProcessT m a b -> ProcessT m a b
