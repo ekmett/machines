e718cc8d1a26fbbabb994008215643c1660cf994
Edward Kmett <ekmett@gmail.com>
2013-02-01 22:20:35 -0500

    Refactored T

--- src/Data/Machine/Tee.hs
+++ src/Data/Machine/Tee.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE TypeOperators #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Data.Machine.Tee
@@ -14,26 +15,61 @@
 module Data.Machine.Tee
   ( -- * Tees
     Tee
-  , T(..)
+  , (:+:)(..)
   , tee
   , addL, addR
   , capL, capR
   ) where
 
 import Control.Applicative
+import Control.Comonad
+import Data.Foldable
+import Data.Functor.Extend
 import Data.Machine.Process
 import Data.Machine.Type
 import Data.Machine.Source
+import Data.Copointed
+import Data.Traversable
 
 -------------------------------------------------------------------------------
 -- Tees
 -------------------------------------------------------------------------------
 
 -- | The input descriptor for a 'Tee' or 'TeeT'.
-data T m n a = L (m a) | R (n a)
+data (m :+: n) a = L (m a) | R (n a)
+
+instance (Functor m, Functor n) => Functor (m :+: n) where
+  fmap f (L m) = L (fmap f m)
+  fmap f (R m) = R (fmap f m)
+
+instance (Foldable m, Foldable n) => Foldable (m :+: n) where
+  foldMap f (L m) = foldMap f m
+  foldMap f (R m) = foldMap f m
+
+instance (Traversable m, Traversable n) => Traversable (m :+: n) where
+  traverse f (L m) = L <$> traverse f m
+  traverse f (R m) = R <$> traverse f m
+
+instance (Copointed m, Copointed n) => Copointed (m :+: n) where
+  copoint (L m) = copoint m
+  copoint (R m) = copoint m
+
+instance (Extend m, Extend n) => Extend (m :+: n) where
+  duplicated (L m) = L (extended L m)
+  duplicated (R m) = R (extended R m)
+  extended f (L m) = L (extended (f . L) m)
+  extended f (R m) = R (extended (f . R) m)
+
+instance (Comonad m, Comonad n) => Comonad (m :+: n) where
+  extract (L m) = extract m
+  extract (R m) = extract m
+  duplicate (L m) = L (extend L m)
+  duplicate (R m) = R (extend R m)
+  extend f (L m) = L (extend (f . L) m)
+  extend f (R m) = R (extend (f . R) m)
 
 -- | A 'Machine' that can read from two input stream in a deterministic manner.
-type Tee a b = Machine (T ((->) a) ((->) b))
+type Tee a b = Machine ((->) a :+: (->) b)
 
 -- | Compose a pair of pipes onto the front of a Tee.
 tee :: Process a a' -> Process b b' -> Tee a' b' c -> Tee a b c
@@ -70,7 +106,7 @@ capR s t = fit cappedT (addR s t)
 {-# INLINE capR #-}
 
 -- | Natural transformation used by 'capL' and 'capR'.
-cappedT :: T f f a -> f a
+cappedT :: (f :+: f) a -> f a
 cappedT (R f) = f
 cappedT (L f) = f
 {-# INLINE cappedT #-}
