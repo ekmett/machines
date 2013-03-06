c7fe97b3cc4ceaca818e04e05e8524a1d19ac62a
Edward Kmett <ekmett@gmail.com>
2013-03-06 14:30:38 -0500

    Implemented a minimal unified Plan/Machine.

--- src/Data/Machine/Plan.hs
+++ src/Data/Machine/Plan.hs
@@ -21,7 +21,6 @@ module Data.Machine.Plan
   (
   -- * Plans
     Plan(..)
-  , yield
   , await
   , awaits
   , request
@@ -46,12 +45,11 @@ import Prelude hiding ((.),id)
 -- | You can 'construct' a 'Plan', turning it into a
 -- 'Data.Machine.Type.Machine'.
 --
-newtype Plan o m a = Plan
+newtype Plan m a = Plan
   { runPlan :: forall r.
-      (a -> r) ->                               -- Done a
-      (o -> r -> r) ->                          -- Yield o (Plan o m a)
-      (forall z. (z -> r) -> m z -> r -> r) ->  -- forall z. Await (z -> Plan o m a) (m z) (Plan o m a)
-      r ->                                      -- Fail
+      (a -> r -> r) ->                          -- Yield a (Plan m a)
+      r ->                                      -- Halt
+      (forall z. m z -> r -> (z -> r) -> r) ->  -- forall z. Await (m z) (Plan m a) (z -> Plan m a)
       r
   }
 
@@ -61,60 +59,60 @@ newtype Plan o m a = Plan
 -- It is perhaps easier to think of 'Plan' in its un-cps'ed form, which would
 -- look like:
 
-instance Functor (Plan o m) where
+instance Functor (Plan m) where
   fmap f (Plan m) = Plan $ \k -> m (k . f)
   {-# INLINE fmap #-}
 
-instance Apply (Plan o m) where
+instance Apply (Plan m) where
   (<.>) = ap
   {-# INLINE (<.>) #-}
 
-instance Applicative (Plan o m) where
-  pure a = Plan $ \kp _ _ _ -> kp a
+instance Applicative (Plan m) where
+  pure a = Plan $ \ky kh _ -> ky a kh
   {-# INLINE pure #-}
   (<*>) = ap
   {-# INLINE (<*>) #-}
 
-instance Alt (Plan o m) where
+instance Alt (Plan m) where
   (<!>) = (<|>)
   {-# INLINE (<!>) #-}
 
-instance Plus (Plan o m) where
+instance Plus (Plan m) where
   zero = empty
   {-# INLINE zero #-}
 
-instance Alternative (Plan o m) where
-  empty = Plan $ \_ _ _ kf -> kf
+instance Alternative (Plan m) where
+  empty = Plan $ \_ kh _ -> kh
   {-# INLINE empty #-}
-  Plan m <|> Plan n = Plan $ \kp ke kr kf -> m kp ke (\ks kir _ -> kr ks kir (n kp ke kr kf)) (n kp ke kr kf)
+  Plan m <|> Plan n = Plan $ \ky kh ka -> m ky (n ky kh ka) ka
   {-# INLINE (<|>) #-}
 
-instance Bind (Plan o m) where
+instance Bind (Plan m) where
   (>>-) = (>>=)
   {-# INLINE (>>-) #-}
 
-instance Monad (Plan o m) where
-  return a = Plan $ \kp _ _ _ -> kp a
+instance Monad (Plan m) where
+  return a = Plan $ \ky kh _ -> ky a kh
   {-# INLINE return #-}
-  Plan m >>= f = Plan $ \kp ke kr kf -> m (\a -> runPlan (f a) kp ke kr kf) ke kr kf
-  fail _ = Plan $ \_ _ _ kf -> kf
+  Plan m >>= f = Plan $ \ky kh ka -> m (\a kn -> runPlan (f a) ky kn ka) kh ka
+  fail _ = Plan $ \_ kh _ -> kh
   {-# INLINE (>>=) #-}
 
-instance MonadPlus (Plan o m) where
+instance MonadPlus (Plan m) where
   mzero = empty
   {-# INLINE mzero #-}
   mplus = (<|>)
   {-# INLINE mplus #-}
 
-instance MonadTrans (Plan o) where
-  lift m = Plan $ \kp _ ka kf -> ka kp m kf
+instance MonadTrans Plan where
+  lift m = Plan $ \ky kh ka -> ka m kh $ \a -> ky a kh
   {-# INLINE lift #-}
 
-instance MonadIO m => MonadIO (Plan o m) where
-  liftIO m = Plan $ \kp _ ka kf -> ka kp (liftIO m) kf
+instance MonadIO m => MonadIO (Plan m) where
+  liftIO m = Plan $ \ky kh ka -> ka (liftIO m) kh $ \a -> ky a kh
   {-# INLINE liftIO #-}
 
-instance MonadState s m => MonadState s (Plan o m) where
+instance MonadState s m => MonadState s (Plan m) where
   get = lift get
   {-# INLINE get #-}
   put = lift . put
@@ -124,22 +122,17 @@ instance MonadState s m => MonadState s (Plan o m) where
   {-# INLINE state #-}
 #endif
 
-instance MonadReader e m => MonadReader e (Plan o m) where
+instance MonadReader e m => MonadReader e (Plan m) where
   ask = lift ask
   {-# INLINE ask #-}
 #if MIN_VERSION_mtl(2,1,0)
   reader = lift . reader
   {-# INLINE reader #-}
 #endif
-  local f (Plan m) = Plan $ \kp ke kr -> m kp ke $ \ar -> kr ar . local f
+  local f (Plan m) = Plan $ \ky kh ka -> m ky kh (ka . local f)
   {-# INLINE local #-}
 
--- | Output a result.
-yield :: o -> Plan o m ()
-yield o = Plan $ \kp ke _ _ -> ke o (kp ())
-{-# INLINE yield #-}
-
-instance Await i m => Await i (Plan o m) where
+instance Await i m => Await i (Plan m) where
   await = awaits await
   {-# INLINE await #-}
 
@@ -151,9 +144,10 @@ instance Await i m => Await i (Plan o m) where
 --- 'awaits' 'This' :: 'Await' i f => 'Plan' o (Y f g) i
 --- 'awaits' 'That' :: 'Await' j g => 'Plan' o (Y f g) j
 --- @
-awaits :: Await i f => (f i -> g j) -> Plan o g j
+awaits :: Await i f => (f i -> g j) -> Plan g j
 awaits f = request (f await)
 {-# INLINE awaits #-}
 
-request :: g j -> Plan o g j
-request m = Plan $ \kp _ ka kf -> ka kp m kf
+request :: m a -> Plan m a
+request m = Plan $ \ky kh ka -> ka m kh $ \a -> ky a kh
+{-# INLINE request #-}
