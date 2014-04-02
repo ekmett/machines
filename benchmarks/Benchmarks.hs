bff1829f2ecdc05eaf885fd597df89b33b0611b1
YoEight <yo.eight@gmail.com>
2014-04-02 23:26:32 +0200

    Refactored benchmarks

--- benchmarks/Benchmarks.hs
+++ benchmarks/Benchmarks.hs
@@ -8,32 +8,28 @@ import qualified Data.Machine      as M
 import qualified Pipes             as P
 import qualified Pipes.Prelude     as P
 
-value :: Integer
+value :: Int
 value = 1000000
 
-runM_ :: M.MachineT Identity k o -> ()
-runM_ m = runIdentity $ M.runT_ m
+drainM :: M.ProcessT Identity Int o -> ()
+drainM m = runIdentity $ M.runT_ (sourceM M.~> m)
 
-runP :: P.Effect Identity a -> a
-runP e = runIdentity $ P.runEffect e
+drainP :: P.Proxy () Int () a Identity () -> ()
+drainP p = runIdentity $ P.runEffect $ P.for (sourceP P.>-> p) P.discard
 
-runC :: C.Source Identity a -> ()
-runC e = runIdentity $ e C.$$ C.sinkNull
+drainC :: C.Conduit Int Identity a -> ()
+drainC c = runIdentity $ (sourceC C.$= c) C.$$ C.sinkNull
+
+sourceM = M.enumerateFromTo 1 value
+sourceC = C.enumFromTo 1 value
+sourceP = P.each [1..value]
 
 main :: IO ()
 main =
   defaultMain
   [ bgroup "mapping"
-      [ bench "machines" $ nf
-        (\s -> runM_ (s M.~> M.auto (+1)))
-        (M.enumerateFromTo 1 value)
-
-      , bench "pipes" $ nf
-        (\n -> runP $ P.for (n P.>-> P.map (+1)) P.discard)
-        (P.each [1..value])
-
-      , bench "conduit" $ nf
-        (\n -> runC (n C.$= C.map (+1)))
-        (C.enumFromTo 1 value)
+      [ bench "machines" $ whnf drainM (M.auto (+1))
+      , bench "pipes" $ whnf drainP (P.map (+1))
+      , bench "conduit" $ whnf drainC (C.map (+1))
       ]
   ]
