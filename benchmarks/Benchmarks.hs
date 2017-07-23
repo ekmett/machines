7f8af84f0215f380d91dd38c77686bad151c922f
Harendra Kumar <harendra.kumar@gmail.com>
2017-07-23 17:00:07 +0530

    Add "streaming" for benchmark comparison
    "streaming" is the closest competitor, faster than "machines" for map,
    dropWhile and last operations.

--- benchmarks/Benchmarks.hs
+++ benchmarks/Benchmarks.hs
@@ -1,6 +1,7 @@
 module Main (main) where
 
 import Control.Applicative
+import Data.Function ((&))
 import Control.Monad (void)
 import Control.Monad.Identity
 import Criterion.Main
@@ -10,6 +11,7 @@ import qualified Data.Conduit.List as C
 import qualified Data.Machine      as M
 import qualified Pipes             as P
 import qualified Pipes.Prelude     as P
+import qualified Streaming.Prelude as S
 import Prelude
 
 value :: Int
@@ -27,61 +29,77 @@ drainC c = runIdentity $ (sourceC C.$= c) C.$$ C.sinkNull
 drainSC :: C.Sink Int Identity b -> ()
 drainSC c = runIdentity $ void $! sourceC C.$$ c
 
+drainS :: (S.Stream (S.Of Int) Identity () -> S.Stream (S.Of Int) Identity ())
+    -> ()
+drainS s = runIdentity $ S.effects $ sourceS & s
+
 sourceM = M.enumerateFromTo 1 value
 sourceC = C.enumFromTo 1 value
 sourceP = P.each [1..value]
+sourceS = S.each [1..value]
 
 main :: IO ()
 main =
   defaultMain
   [ bgroup "map"
       [ bench "machines" $ whnf drainM (M.mapping (+1))
+      , bench "streaming" $ whnf drainS (S.map (+1))
       , bench "pipes" $ whnf drainP (P.map (+1))
       , bench "conduit" $ whnf drainC (C.map (+1))
       ]
   , bgroup "drop"
       [ bench "machines" $ whnf drainM (M.dropping value)
+      , bench "streaming" $ whnf drainS (S.drop value)
       , bench "pipes" $ whnf drainP (P.drop value)
       , bench "conduit" $ whnf drainC (C.drop value)
       ]
   , bgroup "dropWhile"
       [ bench "machines" $ whnf drainM (M.droppingWhile (<= value))
+      , bench "streaming" $ whnf drainS (S.dropWhile (<= value))
       , bench "pipes" $ whnf drainP (P.dropWhile (<= value))
       , bench "conduit" $ whnf drainC (CC.dropWhile (<= value))
       ]
   , bgroup "scan"
       [ bench "machines" $ whnf drainM (M.scan (+) 0)
+      , bench "streaming" $ whnf drainS (S.scan (+) 0 id)
       , bench "pipes" $ whnf drainP (P.scan (+) 0 id)
       , bench "conduit" $ whnf drainC (CC.scanl (+) 0)
       ]
   , bgroup "take"
       [ bench "machines" $ whnf drainM (M.taking value)
+      , bench "streaming" $ whnf drainS (S.take value)
       , bench "pipes" $ whnf drainP (P.take value)
       , bench "conduit" $ whnf drainC (C.isolate value)
       ]
   , bgroup "takeWhile"
       [ bench "machines" $ whnf drainM (M.takingWhile (<= value))
+      , bench "streaming" $ whnf drainS (S.takeWhile (<= value))
       , bench "pipes" $ whnf drainP (P.takeWhile (<= value))
       , bench "conduit" $ whnf drainC (CC.takeWhile (<= value))
       ]
   , bgroup "fold"
       [ bench "machines" $ whnf drainM (M.fold (+) 0)
+      , bench "streaming" $ whnf (S.fold (+) 0 id) sourceS
       , bench "pipes" $ whnf (P.fold (+) 0 id) sourceP
       , bench "conduit" $ whnf drainSC (C.fold (+) 0)
       ]
   , bgroup "filter"
       [ bench "machines" $ whnf drainM (M.filtered even)
+      , bench "streaming" $ whnf drainS (S.filter even)
       , bench "pipes" $ whnf drainP (P.filter even)
       , bench "conduit" $ whnf drainC (C.filter even)
       ]
   , bgroup "mapM"
       [ bench "machines" $ whnf drainM (M.autoM Identity)
+      , bench "streaming" $ whnf drainS (S.mapM Identity)
       , bench "pipes" $ whnf drainP (P.mapM Identity)
       , bench "conduit" $ whnf drainC (C.mapM Identity)
       ]
   , bgroup "zip"
       [ bench "machines" $ whnf (\x -> runIdentity $ M.runT_ x)
           (M.capT sourceM sourceM M.zipping)
+      , bench "streaming" $ whnf (\x -> runIdentity $ S.effects $ x)
+          (S.zip sourceS sourceS)
       , bench "pipes" $ whnf (\x -> runIdentity $ P.runEffect $ P.for x P.discard)
           (P.zip sourceP sourceP)
       , bench "conduit" $ whnf (\x -> runIdentity $ x C.$$ C.sinkNull)
@@ -89,11 +107,13 @@ main =
       ]
   , bgroup "concat"
       [ bench "machines" $ whnf drainM (M.mapping (replicate 10) M.~> M.asParts)
+      , bench "streaming" $ whnf drainS (S.concat . S.map (replicate 10))
       , bench "pipes" $ whnf drainP (P.map (replicate 10) P.>-> P.concat)
       , bench "conduit" $ whnf drainC (C.map (replicate 10) C.$= C.concat)
       ]
   , bgroup "last"
       [ bench "machines" $ whnf drainM (M.final)
+      , bench "streaming" $ whnf S.last sourceS
       , bench "pipes" $ whnf P.last sourceP
       ]
   , bgroup "buffered"
